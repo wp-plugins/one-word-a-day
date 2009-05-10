be8d37799b4ec712964440bf39346255b86f69f1
mcosx <mcosx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-10 15:35:50 +0000

    Moved the holiday data into the setting file.
    git-svn-id: https://plugins.svn.wordpress.org/one-word-a-day/trunk@116909 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- class.owad.php
+++ class.owad.php
@@ -127,44 +127,8 @@ class Owad
 	
 	function is_holiday( $date )
 	{
-		$holidays = array(
-		/*
-		date( "Y-m-d", mktime( 0, 0, 0, 1, 1 ) ),
-		date( "Y-m-d", mktime( 0, 0, 0, 5, 1) ),
-		date( "Y-m-d", mktime( 0, 0, 0, 5, 21 ) ),
-		date( "Y-m-d", mktime( 0, 0, 0, 6, 1 ) ),
-		date( "Y-m-d", mktime( 0, 0, 0, 12, 25 ) )
-		*/
-		
-		// Got these data from owad.de
-		"2009-01-01",  	  //New Year's Day
-		"2009-01-02",     //x-mas-break
-		"2009-01-05",     //x-mas-break
-		"2009-01-06",     //x-mas-break
-		"2009-01-07",     //x-mas-break
-		"2009-01-08",     //x-mas-break
-		"2009-01-09",     //x-mas-break
-		"2009-04-10",     //Karfreitag
-		"2009-04-13",     //Ostermontag
-		"2009-05-01",     //Maifeiertag
-		"2009-05-21",     //Christi Himmelfahrt
-		"2009-06-01",     //Pfingstmontag
-		"2009-10-03",     //Tag der Deutschen Einheit
-		"2009-12-25",     //1. Weihnachtstag
-		"2009-12-26",     //2. Weihnachtstag
-		
-		"2010-01-01",  	  //Neujahr
-		"2010-04-02",     //Karfreitag
-		"2010-04-05",     //Ostermontag
-		"2010-05-01",     //Maifeiertag
-		"2010-05-13",     //Christi Himmelfahrt
-		"2010-05-24",     //Pfingstmontag
-		"2010-10-03",     //Tag der Deutschen Einheit
-		"2010-12-25",     //1. Weihnachtstag
-		"2010-12-26"      //2. Weihnachtstag
-		);
-		
-		return in_array( $date, $holidays );
+		global $owad_holidays;		
+		return in_array( $date, $owad_holidays );
 	}
 	
 	function extract_set( $word )
@@ -394,13 +358,6 @@ class Owad
 				<br/>Please leave a comment <a href="http://slopjong.de/2009/03/20/one-word-a-day/" 
 				target="_blank">here</a> to help improve this widget.';
 	}
-	
-	/*
-	function get_reference()
-	{
-		return $this;
-	}
-	*/
 	
 }
 
