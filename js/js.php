4d3bb3407ba0527cb0570861910c43c814cd982a
mcosx <mcosx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-16 19:34:08 +0000

    Added: Multi-widget support Removed: The auto post generator form ( it will be added to class.owad.php )
    git-svn-id: https://plugins.svn.wordpress.org/one-word-a-day/trunk@126545 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/js.php
+++ js/js.php
@@ -13,24 +13,23 @@ echo <<<JAVASCRIPT
 		var tb_closeImage = "$inc_path/js/thickbox/tb-close.png"
 
 		// Loads the word in the json format		
-		function loadData()
+		function loadData( widget_id )
 		{
-			var dataToBeSent = jQuery('#owad_wordid').serialize();
-			
-			jQuery.getJSON("${url_path}word2json.php", dataToBeSent, function(json){
+			var dataToBeSent = jQuery('#owad_wordid_' + widget_id ).serialize();
 
+			jQuery.getJSON("${url_path}word2json.php", dataToBeSent, function(json){
 				var todays_word = json.todays_word;
 			
-				jQuery("#owad_todays_word")[0].innerHTML = json.todays_word;
+				jQuery("#owad_todays_word_" + widget_id )[0].innerHTML = json.todays_word;
 								
-				jQuery("#owad_alt1")[0].innerHTML = json.alternatives[0];
-				jQuery("#owad_alt1")[0].href = 'http://owad.slopjong.de/'+ escape( todays_word.replace( / /g, "_") ) +'_1' + json.wordid +'.html?KeepThis=true&TB_iframe=true&height=540&width=800';
+				jQuery("#owad_alt1_" + widget_id )[0].innerHTML = json.alternatives[0];
+				jQuery("#owad_alt1_" + widget_id )[0].href = 'http://owad.slopjong.de/'+ escape( todays_word.replace( / /g, "_") ) +'_1' + json.wordid +'.html?KeepThis=true&TB_iframe=true&height=540&width=800';
 								
-				jQuery("#owad_alt2")[0].innerHTML = json.alternatives[1];
-				jQuery("#owad_alt2")[0].href = 'http://owad.slopjong.de/'+ escape( todays_word.replace( / /g, "_") ) +'_3' + json.wordid +'.html?KeepThis=true&TB_iframe=true&height=540&width=800';
+				jQuery("#owad_alt2_" + widget_id )[0].innerHTML = json.alternatives[1];
+				jQuery("#owad_alt2_" + widget_id )[0].href = 'http://owad.slopjong.de/'+ escape( todays_word.replace( / /g, "_") ) +'_3' + json.wordid +'.html?KeepThis=true&TB_iframe=true&height=540&width=800';
 				
-				jQuery("#owad_alt3")[0].innerHTML = json.alternatives[2];
-				jQuery("#owad_alt3")[0].href = 'http://owad.slopjong.de/'+ escape( todays_word.replace( / /g, "_") ) +'_5' + json.wordid +'.html?KeepThis=true&TB_iframe=true&height=540&width=800';
+				jQuery("#owad_alt3_" + widget_id )[0].innerHTML = json.alternatives[2];
+				jQuery("#owad_alt3_" + widget_id )[0].href = 'http://owad.slopjong.de/'+ escape( todays_word.replace( / /g, "_") ) +'_5' + json.wordid +'.html?KeepThis=true&TB_iframe=true&height=540&width=800';
 
 			});
 			
