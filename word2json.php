2d07bbeaea173b5fba1c844bcd51b66f7017e880
mcosx <mcosx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-29 22:40:29 +0000

    Adopted to the new array structure
    git-svn-id: https://plugins.svn.wordpress.org/one-word-a-day/branches/refactoring@130598 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- word2json.php
+++ word2json.php
@@ -2,21 +2,23 @@
 define('OWAD_CACHE_FILE', "cache/words.xml");
 
 include("classes/class.owad.php");
+include("classes/class.model.php");
 
 // This is executed if an ajax request is sent.
 if( isset( $_GET["wordid"] ) )
 { 	
-	$word = Owad_Data::get_cached_word_by_id( intval( $_GET["wordid"] ) );
+	$word = Owad_Model::get_cached_word_by_id( intval( $_GET["wordid"] ) );
 	
 	if( $word == NULL )
 	{
 		// TODO: Adapt the array
 		echo json_encode( array(
-				  "wordid" => "",
-				  "date" => "",
-				  "todays_word" => "",
-				  "alternatives" => array( "", "", "" )
-				  ));
+				"@attributes" => array(
+				  	"wordid" => "",
+				  	"date" => "",
+				  	"content" => "" ),
+				"alternative" => array( "", "", "" )
+				));
 	}
 	else
 		echo json_encode( $word );
