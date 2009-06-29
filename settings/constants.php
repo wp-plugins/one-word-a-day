ef7f043d96ea21d5d7472eabe22725ce79d451b4
mcosx <mcosx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-29 22:37:21 +0000

    Added OWAD_COMMENT_TEXT
    git-svn-id: https://plugins.svn.wordpress.org/one-word-a-day/branches/refactoring@130595 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- settings/constants.php
+++ settings/constants.php
@@ -16,4 +16,5 @@ define('OWAD_FOLDER', str_replace( "/settings", "", dirname(plugin_basename(__FI
 define('OWAD_URLPATH', get_option('siteurl').'/wp-content/plugins/' . OWAD_FOLDER.'/');
 define('OWAD_CACHE_FILE', WP_PLUGIN_DIR .'/'. OWAD_FOLDER . "/cache/words.xml");
 define('OWAD_USE_CACHE', true );
+define('OWAD_COMMENT_TEXT', 'Learning English with the WordPress plugin <em>One Word A Day</em> which displays a new English word and a quiz in the sidebar every work day. <a rel="follow" href="http://slopjong.de/2009/03/20/one-word-a-day/?KeepThis=true&TB_iframe=true&height=540&width=800" class="thickbox" target="_blank">Get it here</a>!' );
 ?>
